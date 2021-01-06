287ca169f19f06a0a7a6cdebfb17cb93275efb32
shiota <mothership0018@gmail.com>
2021-01-07 07:43:24 +0900

    課題提出

--- app/src/main/java/jp/techacademy/yukinori/autoslideshowapp/MainActivity.kt
+++ app/src/main/java/jp/techacademy/yukinori/autoslideshowapp/MainActivity.kt
@@ -9,10 +9,17 @@ import android.util.Log
 import android.provider.MediaStore
 import android.content.ContentUris
 import kotlinx.android.synthetic.main.activity_main.*
+import  android.os.Handler
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
     private val PERMISSIONS_REQUEST_CODE = 100
+
+    private var mTimer: Timer? = null
+    private var mTimerSec = 0.0
+    private var mHandler = Handler()
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -71,12 +78,20 @@ class MainActivity : AppCompatActivity() {
         }
 
         next_button.setOnClickListener {
-            if (cursor!!.moveToNext()) {
+            if (cursor.isLast()) {
+                cursor.moveToFirst()
                 val fieldIndex = cursor.getColumnIndex(MediaStore.Images.Media._ID)
                 val id = cursor.getLong(fieldIndex)
                 val imageUri =
                     ContentUris.withAppendedId(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, id)
-
+                Log.d("ANDROID", "URI :" + imageUri.toString())
+                imageView.setImageURI(imageUri)
+            } else {
+                cursor.moveToNext()
+                val fieldIndex = cursor.getColumnIndex(MediaStore.Images.Media._ID)
+                val id = cursor.getLong(fieldIndex)
+                val imageUri =
+                    ContentUris.withAppendedId(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, id)
                 Log.d("ANDROID", "URI :" + imageUri.toString())
                 imageView.setImageURI(imageUri)
             }
@@ -84,16 +99,67 @@ class MainActivity : AppCompatActivity() {
         }
 
         back_button.setOnClickListener {
-            if (cursor!!.moveToPrevious()) {
+            if (cursor.isFirst()) {
+                cursor.moveToLast()
                 val fieldIndex = cursor.getColumnIndex(MediaStore.Images.Media._ID)
                 val id = cursor.getLong(fieldIndex)
                 val imageUri =
                     ContentUris.withAppendedId(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, id)
-
+                Log.d("ANDROID", "URI :" + imageUri.toString())
+                imageView.setImageURI(imageUri)
+            } else {
+                cursor.moveToPrevious()
+                val fieldIndex = cursor.getColumnIndex(MediaStore.Images.Media._ID)
+                val id = cursor.getLong(fieldIndex)
+                val imageUri =
+                    ContentUris.withAppendedId(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, id)
                 Log.d("ANDROID", "URI :" + imageUri.toString())
                 imageView.setImageURI(imageUri)
             }
 
         }
+
+        start_stop_button.setOnClickListener{
+            if (mTimerSec == 0.0) {
+                start_stop_button.text = "停止"
+                mTimer = Timer()
+                mTimer!!.schedule(object : TimerTask(){
+                    override fun run() {
+                        mTimerSec += 2.0
+                        mHandler.post{
+                            if (cursor.isLast()) {
+                                cursor.moveToFirst()
+                                val fieldIndex = cursor.getColumnIndex(MediaStore.Images.Media._ID)
+                                val id = cursor.getLong(fieldIndex)
+                                val imageUri =
+                                    ContentUris.withAppendedId(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, id)
+                                Log.d("ANDROID", "URI :" + imageUri.toString())
+                                imageView.setImageURI(imageUri)
+                            } else {
+                                cursor.moveToNext()
+                                val fieldIndex = cursor.getColumnIndex(MediaStore.Images.Media._ID)
+                                val id = cursor.getLong(fieldIndex)
+                                val imageUri =
+                                    ContentUris.withAppendedId(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, id)
+                                Log.d("ANDROID", "URI :" + imageUri.toString())
+                                imageView.setImageURI(imageUri)
+                            }
+                        }
+                    }
+                }, 2000, 2000)
+                next_button.isClickable = false
+                back_button.isClickable = false
+            } else {
+                start_stop_button.text = "再生"
+                mTimer!!.cancel()
+                mTimerSec = 0.0
+                next_button.isClickable = true
+                back_button.isClickable = true
+            }
+
+
+        }
+
+
     }
 }
